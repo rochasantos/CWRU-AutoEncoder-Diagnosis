4a59c5ea16637f18e297010d295f9ff1700febf5
Adriano Rocha <adriano_angelo@live.com>
2025-05-25 13:30:51 -0300

    saving best model

--- src/models/cnn1.py
+++ src/models/cnn1.py
@@ -2,35 +2,53 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Simple CNN 1D model
-# This model is a simple 1D CNN with two convolutional layers followed by max pooling 
-# and two fully connected layers.
-class CNN1D(nn.Module):
-    def __init__(self, input_length, num_classes=2):
-        super(CNN1D, self).__init__()
+class BinaryCNN(nn.Module):
+    def __init__(self, input_channels=1, input_length=9600):
+        """
+        Args:
+            input_channels (int): number of input channels.
+            input_length (int): length of the input signal.
+        """
+        super(BinaryCNN, self).__init__()
+        self.input_channels = input_channels
+        self.input_length = input_length
 
-        # self.layer_norm = nn.LayerNorm(input_length)
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=16)
-        self.pool1 = nn.MaxPool1d(kernel_size=16)
+        self.ln = nn.LayerNorm(input_length)
+        self.conv1 = nn.Conv1d(input_channels, out_channels=16, kernel_size=16, padding=1)
+        self.pool1 = nn.MaxPool1d(8)
 
-        self.conv2 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=16)
-        self.pool2 = nn.MaxPool1d(kernel_size=16)
+        self.conv2 = nn.Conv1d(16, out_channels=32, kernel_size=32, padding=1)
+        self.pool2 = nn.MaxPool1d(8)
 
-        self._to_linear = self._get_conv_output(input_length)
+        # Dynamically compute flatten size
+        self.flatten_size = self._get_flatten_size()
 
-        self.fc1 = nn.Linear(self._to_linear, 64)
-        self.fc2 = nn.Linear(64, num_classes)
+        self.fc1 = nn.Linear(self.flatten_size, 64)
+        self.fc2 = nn.Linear(64, 2)
 
-    def _get_conv_output(self, input_length):
-        x = torch.zeros(1, 1, input_length)
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        return x.view(1, -1).shape[1]
+    def _get_flatten_size(self):
+        """
+        Pass a dummy input through conv and pool layers to calculate the flatten size.
+        """
+        with torch.no_grad():
+            x = torch.zeros(1, self.input_channels, self.input_length)
+            x = F.relu(self.conv1(x))
+            x = self.pool1(x)
+            x = F.relu(self.conv2(x))
+            x = self.pool2(x)
+            flatten_size = x.view(1, -1).shape[1]
+        return flatten_size
 
     def forward(self, x):
-        # x = self.layer_norm(x)
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.ln(x)
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        return torch.sigmoid(self.fc2(x))
+        x = self.fc2(x)
+
+        return torch.sigmoid(x)
