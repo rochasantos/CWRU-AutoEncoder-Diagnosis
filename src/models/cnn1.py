4117b76e1ce193a7a77aa5e5401977e53a9a12ab
Adriano Rocha <adriano_angelo@live.com>
2025-05-26 18:15:05 -0300

    binary cnn and hierarchical classifier

--- src/models/cnn1.py
+++ src/models/cnn1.py
@@ -3,23 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BinaryCNN(nn.Module):
-    def __init__(self, input_channels=1, input_length=9600):
-        """
-        Args:
-            input_channels (int): number of input channels.
-            input_length (int): length of the input signal.
-        """
+    def __init__(self, input_channels=1, input_length=9600):        
         super(BinaryCNN, self).__init__()
         self.input_channels = input_channels
         self.input_length = input_length
 
         self.ln = nn.LayerNorm(input_length)
-        self.conv1 = nn.Conv1d(input_channels, out_channels=16, kernel_size=16, padding=1)
-        self.ln1 = nn.LayerNorm([16, 1198])
-        self.pool1 = nn.MaxPool1d(8)
 
+        self.conv1 = nn.Conv1d(input_channels, out_channels=16, kernel_size=16, padding=1)
+        self.pool1 = nn.MaxPool1d(8)
         self.conv2 = nn.Conv1d(16, out_channels=32, kernel_size=32, padding=1)
-        self.ln2 = nn.LayerNorm([32, 146])
         self.pool2 = nn.MaxPool1d(8)
 
         # Dynamically compute flatten size
@@ -45,14 +38,75 @@ class BinaryCNN(nn.Module):
         x = self.ln(x)
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
-        # x = self.ln1(x)
-
         x = F.relu(self.conv2(x))
         x = self.pool2(x)
-        # x = self.ln2(x)
 
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
 
         return torch.sigmoid(x)
+
+
+class HierarchicalClassifier(nn.Module):
+    def __init__(self, clf_n_bio, clf_b_i, clf_b_o, clf_i_o, device='cuda'):
+        super(HierarchicalClassifier, self).__init__()
+        
+        # Classificadores
+        self.clf_n_bio = clf_n_bio.to(device)  # N/BIO
+        self.clf_b_i = clf_b_i.to(device)      # B/I
+        self.clf_b_o = clf_b_o.to(device)      # B/O
+        self.clf_i_o = clf_i_o.to(device)      # I/O
+        
+        self.device = device
+
+    def forward(self, x):
+        """
+        x: Tensor [batch_size, channels, input_length]
+        Retorna: lista de rótulos ['N', 'B', 'I', 'O']
+        """
+        x = x.to(self.device)
+        out_n_bio = self.clf_n_bio(x)
+        pred_n_bio = torch.argmax(out_n_bio, dim=1)  # 0: N, 1: BIO
+
+        predictions = []
+
+        for idx, p in enumerate(pred_n_bio):
+            sample = x[idx].unsqueeze(0)
+
+            if p.item() == 0:
+                predictions.append('N')
+            else:
+                out_b_i = self.clf_b_i(sample)
+                pred_b_i = torch.argmax(out_b_i, dim=1)  # 0: B, 1: I
+
+                if pred_b_i.item() == 0:
+                    out_b_o = self.clf_b_o(sample)
+                    pred_b_o = torch.argmax(out_b_o, dim=1)  # 0: B, 1: O
+
+                    if pred_b_o.item() == 0:
+                        predictions.append('B')
+                    else:
+                        out_i_o = self.clf_i_o(sample)
+                        pred_i_o = torch.argmax(out_i_o, dim=1)  # 0: I, 1: O
+
+                        if pred_i_o.item() == 0:
+                            predictions.append('I')
+                        else:
+                            predictions.append('O')
+                else:
+                    out_b_o = self.clf_b_o(sample)
+                    pred_b_o = torch.argmax(out_b_o, dim=1)  # 0: B, 1: O
+
+                    if pred_b_o.item() == 0:
+                        predictions.append('B')
+                    else:
+                        out_i_o = self.clf_i_o(sample)
+                        pred_i_o = torch.argmax(out_i_o, dim=1)  # 0: I, 1: O
+
+                        if pred_i_o.item() == 0:
+                            predictions.append('I')
+                        else:
+                            predictions.append('O')
+
+        return predictions
